63aa0bbb59988c999cde0c8513d3d7526a21fd86
kotchere <kotchere@udel.edu>
2021-04-06 14:24:08 -0400

    creating framework for functions

---
+++ sheet_summary.js
@@ -0,0 +1,24 @@
+var data
+var sheetID
+
+//funtion to get # of worksheets
+function getWorksheets(params) {
+    document.getElementById("sheetName").innerHTML = sheetID;
+}
+
+//check if worksheets are empty
+function name(params) {
+    
+}
+//how many data points
+function name(params) {
+    getData
+}
+//total incomplete entrie 
+function name(params) {
+    
+}
+//total number of entries
+function name(params) {
+    
+}
