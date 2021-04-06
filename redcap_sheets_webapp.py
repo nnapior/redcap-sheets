9d86afda5d928bc1b917da6faba0ed3f6db31f86
zhenghan-wang <54848986+zhenghan-wang@users.noreply.github.com>
2021-04-06 22:14:49 +0800

    Add files via upload

--- redcap_sheets_webapp.py
+++ redcap_sheets_webapp.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, flash, redirect, url_for
 from py_REDcap import getValues
 from createModifySpreadsheet import *
-from redcapImport import import_redcap
+from redcapImport import import_data, getEvents
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
@@ -13,7 +13,9 @@ def getScripts():
 
 @app.route('/')
 def hello_world():
-   return  render_template('homepage.html')
+   with open("homepage.html", "r") as f:
+       read_data = f.read()
+   return read_data
 
 @app.route('/style.css')
 def getStyle():
@@ -29,13 +31,32 @@ def pushData():
 
 @app.route('/pullData') 
 def pullData():
-    return getValues()
+    data = getValues()
+    return data
 
-@app.route('/import')
+@app.route('/import_page')
+def import_page():
+    events = getEvents()
+    return render_template("importRedcap.html", events = events)
+
+@app.route('/import_redcap', methods = ['PUT','POST'])
 def import_to_redcap():
-    response = import_redcap()
-    for item in response:
-        flash(item)
-    return redirect(url_for("hello_world"))
+    if request.method == "POST":
+        if request.form["importMode"] == 'select':
+            response = import_data(request.form.getlist("events"))
+            if response == False:
+                flash("Please Select Events")
+            else:
+                flash(response)
+            return redirect(url_for("import_page"))
+        elif request.form["importMode"] == "all":
+            response = import_data("All Events")
+            if response == False:
+                flash("Please Select Events")
+            else:
+                flash(response)
+            return redirect(url_for("import_page"))
+
+
 if __name__ == '__main__':
    app.run(debug =True)
