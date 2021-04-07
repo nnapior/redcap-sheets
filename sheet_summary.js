d33f192abd2209bed557344cee006d800b5b9fe8
kotchere <kotchere@udel.edu>
2021-04-07 13:20:58 -0400

    progress

--- sheet_summary.js
+++ sheet_summary.js
@@ -1,24 +1,47 @@
 var data
 var sheetID
+const sheets = google.sheets({version: 'v4', auth});
 
 //funtion to get # of worksheets
 function getWorksheets(params) {
-    document.getElementById("sheetName").innerHTML = sheetID;
+    sheets.spreadsheets.values.get({
+        spreadsheetId: '',
+        range: '',
+      }
+    
 }
 
-//check if worksheets are empty
-function name(params) {
+//function to show sheet summary 
+function sheet_summary(params) {
+    //choose sheet
+    chooseSheet();
+    //check if data is empty "Sheet is empty"
+    const rows = res.data.values;
+    if(rows.length){
+        console.log('Sheet has values');
+    }
+    else{
+        console.log('Sheet is empty, no data found') ;
+    }
+    //Show number of data points 
+    if(rows.length){
+        rows.map((row) => {
+            data_points = 0; 
+            //if row/cell isn't empty add to datapoint counter
+            if(){
+                
+                data_points+=1;
+            }
+            console.log('Number of Data Points: ', data_points);
+        }
+    }
+    //show number of etnries
+
+    //show number of incomplete entries 
     
 }
-//how many data points
-function name(params) {
-    getData
-}
-//total incomplete entrie 
-function name(params) {
-    
-}
-//total number of entries
-function name(params) {
-    
-}
+
+
+
+
+
