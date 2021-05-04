a6b61aaa2846eeebc0dc694ada6eed562bd60d92
kotchere <kotchere@udel.edu>
2021-05-04 14:30:20 -0400

    documentation for py_REDcap_delete.py

--- lib/py_REDcap_delete.py
+++ lib/py_REDcap_delete.py
@@ -3,6 +3,10 @@ from lib.py_REDcap_import import getConfig
 
 
 def delete_records(id):
+    """
+    delete_records
+    Function that deletes all records in redcap id
+    """
     print(id)
     config = getConfig()
     project = Project(config["api_url"], config["api_key"])
